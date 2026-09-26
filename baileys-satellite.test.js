369ed7c412a4af38ca26584019043bc39dc72b69
mundinhocomunicacao-ctrl <mundinhocomunicacao@gmail.com>
2026-09-26 13:30:18 -0300

    test: require dedupe age guard and flow watchdog

--- baileys-satellite.test.js
+++ baileys-satellite.test.js
@@ -8,7 +8,7 @@ for(const token of [
 "DIVA_AUTH_VAULT_SECRET","aes-256-gcm","x-diva-vault-secret","x-diva-installation-id",
 "x-diva-timestamp","x-diva-nonce","x-diva-signature","x-diva-gateway-version",
 "diva-universal-private-gateway-v0.1","stableStringify","conversation_ref","whatsapp://",
-"sock.sendMessage","/pair"
+"sock.sendMessage","/pair","processedMessageIds","messageTimestamp","DIVA_MESSAGE_MAX_AGE_MS","DIVA_FLOW_WATCHDOG_MS","DIVA_FLOW_WATCHDOG_RECYCLE"
 ]) assert.ok(src.includes(token),"missing contract token: "+token);
 assert.ok(!src.includes("useMultiFileAuthState"),"must not use filesystem auth state");
 console.log("DIVA_BAILEYS_SATELLITE_CONTRACT_OK");
