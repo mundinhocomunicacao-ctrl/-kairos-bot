febaa5f8ad21f0aeef5f9aa85398f98cdad708c2
mundinhocomunicacao-ctrl <mundinhocomunicacao@gmail.com>
2026-09-26 13:34:34 -0300

    test: require supervised Baileys recovery

--- baileys-satellite.test.js
+++ baileys-satellite.test.js
@@ -8,7 +8,8 @@ for(const token of [
 "DIVA_AUTH_VAULT_SECRET","aes-256-gcm","x-diva-vault-secret","x-diva-installation-id",
 "x-diva-timestamp","x-diva-nonce","x-diva-signature","x-diva-gateway-version",
 "diva-universal-private-gateway-v0.1","stableStringify","conversation_ref","whatsapp://",
-"sock.sendMessage","/pair","processedMessageIds","messageTimestamp","DIVA_MESSAGE_MAX_AGE_MS","DIVA_FLOW_WATCHDOG_MS","DIVA_FLOW_WATCHDOG_RECYCLE"
+"sock.sendMessage","/pair","processedMessageIds","messageTimestamp","DIVA_MESSAGE_MAX_AGE_MS","DIVA_FLOW_WATCHDOG_MS","DIVA_FLOW_WATCHDOG_RECYCLE","DIVA_SUPERVISED_RESTART","process.exit(1)","bootInProgress"
 ]) assert.ok(src.includes(token),"missing contract token: "+token);
 assert.ok(!src.includes("useMultiFileAuthState"),"must not use filesystem auth state");
 console.log("DIVA_BAILEYS_SATELLITE_CONTRACT_OK");
+assert.ok(!src.includes("setTimeout(connect,2500)"),"must not reconnect Baileys in-process");
